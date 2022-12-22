6b5ac7840f65c62c00bee7e55fcaf2e9a5f38c7f
Pavel P <pavlov.pavel@gmail.com>
2022-12-23 01:53:55 +0300

    Add bigFunctionTest (increases binary by over 1MB from unused function)
    x64: 136KB => 1351KB
    x86: 109KB => 1764KB

--- testLinker.cpp
+++ testLinker.cpp
@@ -4,3 +4,15 @@ void sayHello()
 {
     printf("hello\n");
 }
+
+extern "C"
+{
+    void p4nenc256v32();
+    void p4ndec256v32();
+}
+
+void bigFunctionTest()
+{
+    p4nenc256v32();
+    p4ndec256v32();
+}
